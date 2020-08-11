28365593509417eb7e1a7d958421297eec2895fe
Igor Berlenko <igor@berlenko.com>
2020-08-11 17:47:10 +0800

    refactor & support identity requests

--- lib/eosio_signing_request.dart
+++ lib/eosio_signing_request.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: omit_local_variable_types, todo
+
 import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:archive/archive.dart';
 import 'package:convert/convert.dart';
@@ -8,6 +11,10 @@ import 'package:eosdart/src/serialize.dart' as ser;
 
 import './decode_string_request.dart';
 import './request_abi.dart';
+import 'request_abi.dart';
+
+String actorPlaceholder = '............1';
+String permissionPlaceholder = '............2';
 
 class EosioSigningRequest {
   EOSClient client;
@@ -19,83 +26,106 @@ class EosioSigningRequest {
 
   EosioSigningRequest(this.client, this.account, this.permission);
 
-  Map<String, dynamic> fillDataPlaceholders(
-      Map<String, dynamic> request, String accountName) {
-    Map<String, dynamic> result = {};
-
-    request.forEach((key, value) {
-      if (value == '............1') {
-        result[key] = accountName;
-      } else {
-        result[key] = value;
-      }
-    });
-
-    return result;
-  }
-
   Future<dynamic> push() async {
     return await client.pushTransaction(Transaction()..actions = [action]);
   }
 
   static Future<EosioSigningRequest> factory(
       EOSClient client, String esrUri, String account,
-      {permission = 'active'}) async {
+      {permission = 'active', Abi contractAbi}) async {
     var esr = await EosioSigningRequest(client, account, permission);
 
     esrUri = esrUri.substring(4);
-    var decodedEsr = decodeStringRequest(esrUri);
-    var esrBytes = Inflate(decodedEsr.sublist(1)).getBytes();
-    var esrTypes = ser.getTypesFromAbi(
-        ser.createInitialTypes(), Abi.fromJson(json.decode(requestAbi)));
-    var esrType = esrTypes['signing_request'];
 
-    dynamic esrMap = esrType.deserialize(esrType, ser.SerialBuffer(esrBytes));
+    final Uint8List decodedUri = decodeStringRequest(esrUri);
 
-    var actionMap;
-    if (esrMap['req'][1] is List) {
-      actionMap = esrMap['req'][1][0];
-    } else {
-      actionMap = esrMap['req'][1];
+    final List<int> bytes = Inflate(decodedUri.sublist(1)).getBytes();
+
+    final Map<String, Type> types = ser.getTypesFromAbi(
+      ser.createInitialTypes(),
+      Abi.fromJson(json.decode(requestAbi)),
+    );
+
+    final Type request = types['signing_request'];
+
+    final dynamic fullData =
+        request.deserialize(request, ser.SerialBuffer(bytes));
+
+    if (fullData['req'][0] == 'identity') {
+      esr.action = Action()
+        ..account = fullData['req'][1].actions[0]['account']
+        ..name = fullData['req'][1].actions[0]['name']
+        ..authorization = fullData['req'][1].actions[0]['authorization']
+        ..data = fullData['req'][1].actions[0]['data'];
+
+      return esr;
     }
 
-    var abi = await client.getRawAbi(actionMap['account']);
-    var types = ser.getTypesFromAbi(ser.createInitialTypes(), abi.abi);
+    dynamic data;
 
-    var actions = Map<String, Type>();
-    for (var act in abi.abi.actions) {
-      actions[act.name] = ser.getType(types, act.type);
-      if (act.name == actionMap['name']) {
-        esr.ricardian = act.ricardian_contract;
-      }
+    switch (fullData['req'][0]) {
+      case 'action':
+        data = fullData['req'][1];
+        break;
+      case 'action[]':
+        data = fullData['req'][1][0];
+        break;
+      case 'transaction':
+        data = fullData['req'][1].actions[0];
+        break;
     }
-    var contract = Contract(types, actions);
-    var actionObj = contract.actions[actionMap['name']];
-    var decodedData = actionObj.deserialize(
-        actionObj, ser.SerialBuffer(hex.decode(actionMap['data'])));
 
-    decodedData.forEach((key, value) {
-      if (value == '............1') {
-        decodedData[key] = account;
-      } else {
-        decodedData[key] = value;
-      }
-    });
+    final action = Action()
+      ..account = data['account']
+      ..name = data['name']
+      ..authorization = data['authorization']
+      ..data = data['data'];
+
+    if (contractAbi == null) {
+      var abiResponse = await client.getRawAbi(action.account);
+      contractAbi = abiResponse.abi;
+    }
+
+    Map<String, Type> contractTypes =
+        ser.getTypesFromAbi(ser.createInitialTypes(), contractAbi);
+
+    Map<String, Type> contractActions = {};
+    for (var act in contractAbi.actions) {
+      contractActions[act.name] = ser.getType(contractTypes, act.type);
+    }
+
+    esr.ricardian = contractAbi.actions
+        .where((e) => e.name == action.name)
+        .first
+        .ricardian_contract;
+
+    final contract = Contract(contractTypes, contractActions);
+
+    Type actionType = contract.actions[action.name];
+
+    Map<String, String> actionData = actionType.deserialize(
+        actionType, ser.SerialBuffer(hex.decode(action.data)));
+
+    actionData = actionData.map((key, value) => MapEntry<String, String>(
+          key,
+          value == actorPlaceholder ? account : value,
+        ));
 
     esr.action = Action()
-      ..account = actionMap['account']
-      ..name = actionMap['name']
-      ..authorization = [Authorization()];
+      ..account = action.account
+      ..name = action.name
+      ..authorization = [
+        Authorization()
+          ..actor = action.authorization[0].actor == actorPlaceholder
+              ? account
+              : action.authorization[0].actor
+          ..permission =
+              action.authorization[0].permission == permissionPlaceholder
+                  ? permission
+                  : action.authorization[0].permission
+      ]
+      ..data = actionData;
 
-    if (actionMap['authorization'][0]['actor'] == '............1') {
-      esr.action.authorization[0].actor = account;
-    }
-
-    if (actionMap['authorization'][0]['permission'] == '............2') {
-      esr.action.authorization[0].permission = permission;
-    }
-
-    esr.action.data = decodedData;
     return esr;
   }
 }
